c9076226cc9513a98e0a9038f92d6c96b2918dbc
anastasiafateeva <a.fateeva@energo.app>
2026-09-26 13:09:44 +0300

    add port from env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/fateevanastusha/videos-go-backend/internal/server"
 )
 
 func main() {
 	address := "localhost:3005"
+	if port := os.Getenv("PORT"); port != "" {
+		address = ":" + port
+	}
 	if err := server.Start(address); err != nil {
 		log.Fatal(err)
 	}
